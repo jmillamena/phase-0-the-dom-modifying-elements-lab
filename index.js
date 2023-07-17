12475df0b65361b74b8451e19c68f424662b38bf
Jenna Millamena <jcmillamena@gmail.com>
2023-07-16 17:15:04 -1000

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.removeAttribute('id');
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+var body = document.querySelector('body');
+body.appendChild(newHeader)
+document.getElementsByTagName("h1")[0].innerHTML="Momo is the champion!";
+
+
+
+
+
+
